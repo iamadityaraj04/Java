9ef918c973eab20b54be2ed23104ade63749d956
iamadityaraj04 <adityarohitraj@gmail.com>
2022-01-22 19:24:05 +0530

    Strings

---
+++ java11.java
@@ -0,0 +1,26 @@
+public class java11{
+
+	public static void main(String[] args) {
+		
+		// Interned String
+		String name1 = "John Watson"; // this remains in the memory till program is not finished
+		
+		// Object String
+		String name2 = new String("Fionna Flynn"); // this can be deleted form the memory by garbage collector
+		
+        // name1 and name2 are known as reference variables
+		// When we print Strings, reference variables rather than showing the hashcodes
+		// they show the data directly
+		System.out.println("name1 is: "+name1);
+		System.out.println("name2 is: "+name2);
+		
+		// Let us print the name1 and name2 with execution of a method called toString()
+		System.out.println("name1.toString() is: "+name1.toString());
+		System.out.println("name2.toString() is: "+name2.toString());
+		
+		// i.e. when we print reference variable of String, 
+		// toString will be executed and it shoes the data rather than hashcodes
+		// Lastly, toString gets executed automatically whenever we print reference variable of String 
+	}
+
+}
