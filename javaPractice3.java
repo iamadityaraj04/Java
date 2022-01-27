0212df83d7af3610f1faad7a693d00c35bbb63ec
iamadityaraj04 <adityarohitraj@gmail.com>
2022-01-27 14:05:42 +0530

    restaurant question

--- javaPractice3.java
+++ javaPractice3.java
@@ -1,49 +1,61 @@
 // Assignment: Create an Object either Restraunt Object or Product from Amazon
 //			 list all the attributes and then create objects :)
+import java.util.Scanner;
+class restaurant{
 
-class restraunt{
+    private String restaurantName;
+    private String restaurantType;
+    private String restaurantLocation;
 
-    private String restrauntName;
-    private String restrauntType;
-    private String restrauntLocation;
-
-    /*void setrestrauntName(String name){
-        this.restrauntName=name;
+    /*void setrestaurantName(String name){
+        this.restaurantName=name;
     }
-    void setrestrauntType(String type){
-        this.restrauntType=type;
+    void setrestaurantType(String type){
+        this.restaurantType=type;
     }
-    void setrestrauntLocation(String location){
-        this.restrauntLocation=location;
+    void setrestaurantLocation(String location){
+        this.restaurantLocation=location;
     }*/
     
-    restraunt(String name, String type, String location ){
-        this.restrauntName=name;
-        this.restrauntType=type;
-        this.restrauntLocation=location;
+    restaurant(String name, String type, String location ){
+        this.restaurantName=name;
+        this.restaurantType=type;
+        this.restaurantLocation=location;
 
     }
 
-    String getRestrauntName() {
-        return restrauntName;
+    String getRestaurantName() {
+        return restaurantName;
     }
-    String getRestrauntType() {
-        return restrauntType;
+    String getRestaurantType() {
+        return restaurantType;
     }
-    String getRestrauntLocation() {
-        return restrauntLocation;
+    String getRestaurantLocation() {
+        return restaurantLocation;
     }
 }
 
 public class javaPractice3 {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         
-        restraunt r1=new restraunt("Sonotel", "Vegitarian", "Ozone Galleria Mall, Dhanbad");
-        // r1.setrestrauntName("Sonotel");
-        // r1.setrestrauntType("Vegitarian");
-        // r1.setrestrauntLocation("Ozone Galleria Mall, Dhanbad");
-        System.out.println("Name: "+r1.getRestrauntName());
-        System.out.println("Type: "+r1.getRestrauntType());
-        System.out.println("Location: "+r1.getRestrauntLocation());
+        System.out.print("Enter restaurant name : ");
+        String name=sc.nextLine();
+        
+        System.out.print("Enter restaurant type : ");
+        String type=sc.nextLine();
+        
+        System.out.print("Enter restaurant location : ");
+        String location=sc.nextLine();
+        
+        sc.close();
+        
+        restaurant r1=new restaurant(name, type, location);
+        // r1.setrestaurantName("Sonotel");
+        // r1.setrestaurantType("Vegitarian");
+        // r1.setrestaurantLocation("Ozone Galleria Mall, Dhanbad");
+        System.out.println("Name: "+r1.getRestaurantName());
+        System.out.println("Type: "+r1.getRestaurantType());
+        System.out.println("Location: "+r1.getRestaurantLocation());
     }
 }
