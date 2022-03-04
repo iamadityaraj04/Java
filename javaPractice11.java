b356e04fe65004270ea7a93ce0b4cf695ca661ae
iamadityaraj04 <adityarohitraj@gmail.com>
2022-03-04 20:51:46 +0530

    binary search

--- javaPractice11.java
+++ javaPractice11.java
@@ -1,10 +1,12 @@
+//binary Searching
+
 import java.util.Scanner;
 public class javaPractice11
 {
 	public static int search(int arr[], int key){
 		int low=0;
 		int high=arr.length-1;
-		while(low<high){
+		while(low<=high){
 			int mid=(low+high)/2;
 			if(arr[mid]==key){
 				return mid;
